1af2e21693a2cc6ee65c6d83d9023c0c2608de73
Gleibson Silva <103868998+GleibsonSilvaJJ@users.noreply.github.com>
2022-07-29 00:39:36 -0300

    Escolha de ambiente para JavaScript
    Atividade proposta: Com ajuda da leitura complementar, você deve configurar o seu ambiente de desenvolvimento JavaScript. Esse ambiente pode ser uma IDE ou um editor de código-fonte. Para isso, você deve levar em consideração alguns pontos: 
    
    O seu ambiente precisa programar na linguagem JavaScript; 
    Se necessário, configure seu ambiente; 
    Deixe ele pronto para uso, ou seja, sem mais configurações. 
    
    Escreva, no mínimo, um parágrafo mostrando seu ambiente configurado, explique sua escolha.

---
+++ escolhaDeAmbiente/escolhaDeAmbiente.js
@@ -0,0 +1,20 @@
+/*
+  Ecolhe o Editor de Texto VScode
+
+        As necessidades já são supridas pelo enorme número de extensões disponíveis. E bem customizável.
+      Você pode alterar a aparência dele manualmente ou baixar temas na loja de extensões.
+*/
+/*
+      O VS Code possui vários atalhos de teclado que facilitam o nosso trabalho. A saber, esses atalhos podem ser customizados. Além disso, você pode atribuir teclas de atalho para funcionalidades existentes mas que não possuem um atalho definido.
+
+      nesse link do Linkedin tem váraios atalhos do VScode https://www.linkedin.com/pulse/todos-os-atalhos-do-vs-code-mateus-barbosa/?originalSubdomain=pt
+
+
+  Por que usar o VS Code?
+      Ferramenta simples;
+      Código aberto;
+      Arquitetura bem planejada;
+      Possibilidade de customização;
+      Várias funcionalidades e atalhos;
+      Rica loja de extensões;
+*/
